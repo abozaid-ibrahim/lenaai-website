04db7d2b79286ef3a088b3d26013e5cd77ebe92e
MahmoudHammam1 <mahmoud.hammam@lenaai.net>
2025-02-08 14:04:44 +0200

    feat(chatbot): Enhance chatbot with dynamic response capabilities

--- js/index.js
+++ js/index.js
@@ -14,6 +14,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(updateCountdown, 1000);
 
+    $("textarea").each(function () {
+        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+    }).on("input", function () {
+        this.style.height = 0;
+        this.style.height = (this.scrollHeight) + "px";
+    });
+
+    const inputField = document.getElementById("user-input");
+    const originalHeight = inputField.style.height || inputField.offsetHeight + "px";
+    inputField.addEventListener("keydown", function (event) {
+        if (event.key === "Enter" && !event.shiftKey) {
+            event.preventDefault();
+            takeUserInput();
+            this.value = "";
+            this.style.height = originalHeight;
+            this.setAttribute("placeholder", "Type Something Here");
+        }
+    });
+
+    let responseProperties;
+
     // Handle user input for Chat-Bot
     const chatSubmit = document.getElementById("submit-btn");
     const inputForm = document.getElementById("input-form");
@@ -25,6 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let userInput = inputField.value.trim();
         const messagesDiv = document.getElementById("messages");
         let message = document.createElement("div");
+        let typingIndicatorDiv = document.createElement("div");
 
 
         if (userInput === "") return;
@@ -44,20 +66,51 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         message.innerHTML = userInput;
         message.classList.add("message", "sent");
+        typingIndicatorDiv.innerHTML = `
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>`;
+        typingIndicatorDiv.classList.add("typing-indicator", "message");
         messagesDiv.appendChild(message);
+        messagesDiv.appendChild(typingIndicatorDiv);
 
-        // $.ajax({
-        //     url: "https://lenaai.net/search",
-        //     method: "POST",
-        //     contentType: "application/json",
-        //     data: JSON.stringify({"query": userInput}),
-        //     success: function (response) {
-        //         console.log(response);
-        //     },
-        //     error: function(xhr, status, error) {
-        //         console.error("Error:", error);
-        //     }
-        // })
+        $.ajax({
+            url: "http://35.225.79.225/chat/",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "query": userInput,
+                "phone_number": "01212121212"
+            }),
+            success: function (response) {
+                let chatResponse = document.createElement("div");
+                chatResponse.innerHTML = response.message;
+                chatResponse.classList.add("message");
+
+                let imagesDiv;
+
+                if (response.properties && response.properties.length > 0) {
+                    responseProperties = response.properties[0];
+                    imagesDiv = document.createElement("div");
+                    imagesDiv.classList.add("images");
+
+                    responseImages = response.properties[0].images;
+                    responseImages.forEach(imageUrl => {
+                        let image = document.createElement("div");
+                        image.classList.add("chat-image");
+                        image.innerHTML = `<img src="${imageUrl}" alt="Property Image">`;
+                        imagesDiv.appendChild(image);
+                    });
+                }
+                messagesDiv.removeChild(typingIndicatorDiv);
+                messagesDiv.appendChild(chatResponse);
+                messagesDiv.appendChild(imagesDiv);
+                messagesDiv.scrollTop = messagesDiv.scrollHeight;
+            },
+            error: function (xhr, status, error) {
+                console.error("Error:", error);
+            }
+        })
 
         inputField.value = "";
         messagesDiv.scrollTop = messagesDiv.scrollHeight;
@@ -93,7 +146,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const messagesDiv = document.getElementById("messages");
 
                 messagesDiv.style.height = "450px";
-                reader.onload = function(e) {
+                reader.onload = function (e) {
                     $('.chat-preview').html(`<img src="${e.target.result}" alt="Chat Preview">`);
                 };
                 reader.readAsDataURL(file);
@@ -144,7 +197,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <input type="submit" value="Post" id="post-btn">
                 </form>
             </div>`;
-        
+
         document.querySelector(".close-btn").addEventListener("click", function () {
             uploadOverlay.style.display = "none";
             uploadDetails.innerHTML = '';
@@ -170,7 +223,7 @@ document.addEventListener("DOMContentLoaded", function () {
             file = fileField.files[0];
             if (file) {
                 const reader = new FileReader();
-                reader.onload = function(e) {
+                reader.onload = function (e) {
                     $('.photo-preview').html(`<img src="${e.target.result}" alt="Photo Preview">`);
                 };
                 reader.readAsDataURL(file);
@@ -182,26 +235,26 @@ document.addEventListener("DOMContentLoaded", function () {
         async function uploadPhoto(file) {
             const formData = new FormData();
             formData.append("file", file);
-        
+
             const response = await fetch("http://localhost:8000/upload_photo", {
                 method: "POST",
                 body: formData
             });
-        
+
             const data = await response.json();
             return data.photo_url; // Return the uploaded photo URL
         }
-        
+
         postButton.addEventListener("click", async function (event) {
             event.preventDefault();
-        
+
             const postContent = document.getElementById("post-content").value;
             let photoUrl = null;
 
             if (file) {
                 photoUrl = await uploadPhoto(file); // Upload and get URL
             }
-        
+
             $.ajax({
                 url: "http://localhost:8000/fb_post",
                 method: "POST",
@@ -218,12 +271,12 @@ document.addEventListener("DOMContentLoaded", function () {
             uploadOverlay.style.display = "none";
         });
     });
-  
+
     // Chat engine list
     const currentChatEngine = document.querySelector(".current-engine");
     const dropdown = document.getElementById('dropdown-menu');
     const chatEngines = Array.from(document.getElementsByClassName("engine-item"));
-    
+
     function toggleDropdown() {
         const isVisible = dropdown.style.display === "block";
         dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
@@ -259,131 +312,126 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Chat images select
-    const chatImages = Array.from(document.getElementsByClassName("chat-image"));
-    const imagesDiv = document.querySelector(".images");
     const galleryOverlay = document.querySelector(".gallery-overlay");
     const gallerycontent = document.querySelector(".gallery-content");
-  
 
-    chatImages.forEach((image) => {
-        image = image.querySelector("img");
-        image.addEventListener("click", function () {
-            galleryOverlay.style.display = "flex";
-            gallerycontent.innerHTML = `
+
+    document.getElementById("messages").addEventListener("click", function (event) {
+        const clickedImage = event.target.closest(".images");
+
+        if (!clickedImage) return;
+
+        galleryOverlay.style.display = "flex";
+        const allImages = Array.from(document.querySelectorAll(".chat-image img")).map(img => img.src);
+        const imageSlides = allImages.map(src => `
+            <div class="slide">
+                <img src="${src}" alt="Chat Image">
+            </div>
+        `).join("");
+        let imageTitle;
+        if (responseProperties) {
+            const compound = responseProperties.compound;
+            const zone = responseProperties.zone;
+            const phase = responseProperties.phase;
+            imageTitle = `${compound}: ${zone}: ${phase}`;
+        } else {
+            imageTitle = "Image Viewer"
+        }
+
+        gallerycontent.innerHTML = `
             <div class="upload-header">
-                <h1>Image Tile</h1>
+                <h1>${imageTitle}</h1>
                 <i class="fa-solid close-btn fa-circle-xmark"></i>
             </div>
             <div class="slider">
                 <div class="slider-wrapper">
-                    <i class="bi bi-hand-thumbs-up like"></i>
-                    <i class="bi bi-hand-thumbs-down dislike"></i>
-                    <div class="slide">
-                        <img src="assets/logo.png">
-                    </div>
-                    <div class="slide">
-                        <img src="assets/careers2.png">
-                    </div>
-                    <div class="slide">
-                        <img src="assets/logo.png">
-                    </div>
-                    <div class="slide">
-                        <img src="assets/careers2.png">
-                    </div>
+                    ${imageSlides}
                 </div>
-
                 <div id="prev"><i class="fa-solid iconCall fa-chevron-left"></i></div>
                 <div id="next"><i class="fa-solid iconCall fa-chevron-right"></i></div>
 
                 <i class="bi bi-hand-thumbs-up like"></i>
                 <i class="bi bi-hand-thumbs-down dislike"></i>
+            </div>
+        `;
 
-                <div class="content">
-                    <p>
-                        Find your dream home today!<br>
-                        Explore top properties at unbeatable prices.
-                    </p>
-                </div>
-            </div>`;
-
-            document.querySelector(".close-btn").addEventListener("click", function () {
-                galleryOverlay.style.display = "none";
-                gallerycontent.innerHTML = '';
-            });
-
-            const slides = document.getElementsByClassName("slide");
-            const sliderWrapper = document.querySelector('.slider-wrapper');
-            const prev = document.getElementById("prev");
-            const next = document.getElementById("next");
-            let slideIndex = 0;
-
-            showSlides(slideIndex);
-
-            function plusSlides(num) {
-                showSlides(slideIndex += num);
-            }
-        
-            function showSlides(num) {      
-                slideIndex = (num + slides.length) % slides.length;
-                if (slideIndex === 0) {
-                    prev.style.display = "none";
-                } else {
-                    prev.style.display = "block";
-                }
-
-                if (slideIndex === slides.length - 1) {
-                    next.style.display = "none";
-                } else {
-                    next.style.display = "block";
-                }
-
-                sliderWrapper.style.transform = `translateX(-${slideIndex * 100}%)`;
-            }
-
-            prev.addEventListener("click", function () {
-                plusSlides(-1);
-            });
-        
-            next.addEventListener("click", function () {
-                plusSlides(1);
-            });
-
-            // like & dislike buttons
-            const likeButtons = Array.from(document.getElementsByClassName("like"));
-            const dislikeButtons = Array.from(document.getElementsByClassName("dislike"));
-
-            likeButtons.forEach((like) => {
-                like.addEventListener("click", function () {
-                    let liked = document.createElement("i");
-                    liked.classList.add("bi", "bi-hand-thumbs-up-fill", "liked");
-
-                    liked.addEventListener("click", function() {
-                        liked.replaceWith(like);
-                    });
-
-                    setTimeout(() => document.querySelector(".close-btn").click(), 1000);
-
-                    like.replaceWith(liked);
-                });
-            });
-
-            dislikeButtons.forEach((dislike) => {
-                dislike.addEventListener("click", function () {
-                    let disliked = document.createElement("i");
-                    disliked.classList.add("bi", "bi-hand-thumbs-down-fill", "disliked");
-
-                    disliked.addEventListener("click", function() {
-                        disliked.replaceWith(dislike);
-                    });
-
-                    dislike.replaceWith(disliked);
-                });
-            });
-
-            // // const imageSource = image.getAttribute("src");
-            // imagesDiv.innerHTML = `<div class="img-message">
-            //     <img src="${imageSource}" alt="Chat Image">
-            // </div>`;
+        document.querySelector(".close-btn").addEventListener("click", function () {
+            galleryOverlay.style.display = "none";
+            gallerycontent.innerHTML = '';
         });
+
+        const slides = document.getElementsByClassName("slide");
+        const sliderWrapper = document.querySelector('.slider-wrapper');
+        const prev = document.getElementById("prev");
+        const next = document.getElementById("next");
+        let slideIndex = 0;
+
+        showSlides(slideIndex);
+
+        function plusSlides(num) {
+            showSlides(slideIndex += num);
+        }
+
+        function showSlides(num) {
+            slideIndex = (num + slides.length) % slides.length;
+            if (slideIndex === 0) {
+                prev.style.display = "none";
+            } else {
+                prev.style.display = "block";
+            }
+
+            if (slideIndex === slides.length - 1) {
+                next.style.display = "none";
+            } else {
+                next.style.display = "block";
+            }
+
+            sliderWrapper.style.transform = `translateX(-${slideIndex * 100}%)`;
+        }
+
+        prev.addEventListener("click", function () {
+            plusSlides(-1);
+        });
+
+        next.addEventListener("click", function () {
+            plusSlides(1);
+        });
+
+        // like & dislike buttons
+        const likeButtons = Array.from(document.getElementsByClassName("like"));
+        const dislikeButtons = Array.from(document.getElementsByClassName("dislike"));
+
+        likeButtons.forEach((like) => {
+            like.addEventListener("click", function () {
+                let liked = document.createElement("i");
+                liked.classList.add("bi", "bi-hand-thumbs-up-fill", "liked");
+
+                liked.addEventListener("click", function () {
+                    liked.replaceWith(like);
+                });
+
+                // setTimeout(() => document.querySelector(".close-btn").click(), 1000);
+
+                like.replaceWith(liked);
+            });
+        });
+
+        dislikeButtons.forEach((dislike) => {
+            dislike.addEventListener("click", function () {
+                let disliked = document.createElement("i");
+                disliked.classList.add("bi", "bi-hand-thumbs-down-fill", "disliked");
+
+                disliked.addEventListener("click", function () {
+                    disliked.replaceWith(dislike);
+                });
+
+                dislike.replaceWith(disliked);
+            });
+        });
+
+        // // const imageSource = image.getAttribute("src");
+        // imagesDiv.innerHTML = `<div class="img-message">
+        //     <img src="${imageSource}" alt="Chat Image">
+        // </div>`;
     });
 })
