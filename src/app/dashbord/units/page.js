1518f0aa581f8176de84b0f2c9ef16bd7fd42e21
hissine hamada <105513061+hissine72@users.noreply.github.com>
2025-04-06 16:45:35 +0200

    first-commit

---
+++ src/app/dashbord/units/page.js
@@ -0,0 +1,13 @@
+import Units from '@/components/dashbord/pages/Units'
+import React from 'react'
+
+const page = () => {
+    
+  return (
+    <div>
+      <Units/>
+    </div>
+  )
+}
+
+export default page
