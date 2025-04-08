bcf193b0e02413f1a52ea27cb8840ce55aa6888f
alaahosnii <alaahosnii1000@gmail.com>
2025-04-08 04:17:51 +0200

    enhanced seo by adding pages metadata and enhanced ssr for some pages

--- src/app/dashbord/units/page.jsx
+++ src/app/dashbord/units/page.jsx
@@ -2,8 +2,12 @@ import Units from '@/components/dashbord/pages/Units'
 import { fetchcombounds, fetchUnits } from '@/components/services/serviceFetching';
 import React from 'react'
 
-
-const page = async () => {
+export const revalidate = 0;
+export const metadata = {
+  title: 'Units',
+  description: 'Units page',
+}
+const Page = async () => {
   // Fetch units data using the server action
   const unitsData = await fetchUnits();
   const comboundata = await fetchcombounds();
@@ -15,4 +19,4 @@ const page = async () => {
   )
 }
 
-export default page
+export default Page
