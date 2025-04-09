12f83aed276474bc2e4fd4f991c459cb814f9fde
Amr-Guaily <amrguaily@gmail.com>
2025-04-10 00:51:11 +0200

    Remove duplicate files

--- public/locales/en.js
+++ public/locales/en.js
@@ -4,4 +4,10 @@ export default {
   language: "Language",
   english: "English",
   arabic: "Arabic",
+  "hero.title1": "MAKE IT AI",
+  "hero.subtitle1": "EFFICIENT & COST-EFFECTIVE",
+  "hero.title2": "SMART REAL ESTATE CRM",
+  "hero.subtitle2": "AUTOMATE & SCALE EFFORTLESSLY",
+  "button.tryChat": "Try our Chatbot",
+  "button.requestDemo": "Request Demo",
 };
