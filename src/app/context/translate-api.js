99d0caed7b35c7fd697983fe6b5626aa3ff0629e
Amr-Guaily <amrguaily@gmail.com>
2025-04-10 18:30:27 +0200

    Refactor I18nProvider and Header component for improved localization handling

--- src/app/context/translate-api.js
+++ src/app/context/translate-api.js
@@ -21,11 +21,12 @@ export const I18nProvider = ({ initialLocal = "ar", children }) => {
      * INFO: Hydration error:
      *   The hydration error occurs because `I18nProvider` is a client-side component, 
      *   but it’s being used in a server-rendered context (the root layout) WITHOUT proper synchronization between server and client states.
-     */
+    */
 
     // Sync with cookie on mount (client-side only)
     useEffect(() => {
         const cookieLang = Cookies.get('lang');
+        console.log(cookieLang, "initialLocal");
         if (cookieLang && cookieLang !== locale) {
             setLocale(cookieLang);
         }
@@ -38,7 +39,7 @@ export const I18nProvider = ({ initialLocal = "ar", children }) => {
         document.documentElement.dir = lang === "ar" ? "rtl" : "ltr";
     }, []);
 
-    const t = useMemo(() => locale === "en" ? en : ar, [locale]);
+    const t = useMemo(() => locale === "ar" ? ar : en, [locale]);
 
     return (
         <I18nContext.Provider value={{ locale, t, changeLanguage }}>
